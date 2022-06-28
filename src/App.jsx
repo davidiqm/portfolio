9b7967ee74ede66fa7406aff0cc6f8d6fda356a7
David Quime <al061737@uacam.mx>
2022-06-27 19:45:50 -0500

    Contact component agregado

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Education } from './components/Education/Education';
 import { Skillset } from './components/Skillset/Skillset';
 import { Projects } from './components/Projects/Projects';
 import { Footer } from './components/Footer/Footer';
+import { Contact } from './components/Contact/Contact';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Projects />
       <Education />
       <Skillset />
+      <Contact />
       <Footer />
     </div>
   )
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+export function Contact() {
+  return <>
+    <div class="contact-container">
+      <h1 class="contact-title">Contáctame</h1>
+      <form action="" class="contact-form" method="get">
+        <p>
+          <label for="name">Nombre:</label>
+          <input type="text" id="name" />
+        </p>
+          <button type="submit">Enviar</button>
+      </form>
+    </div>
+  </>
+}
